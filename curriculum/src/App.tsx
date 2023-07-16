fb9c659053381a8f53f711e1da668e7b8ec0be86
AmandaFI <a.ferrari.iaquinta@gmail.com>
2023-07-16 19:18:42 -0300

    First draft.

--- curriculum/src/App.tsx
+++ curriculum/src/App.tsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { List, ListItem, ListItemButton, ListItemText, Tab, Tabs } from "@mui/material";
+import { VscChevronDown } from "react-icons/vsc";
+import Contact from "./components/contact";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	let currentTab = "draft";
+	let abc = (
+		<Contact />
+		// <Box sx={{ bgcolor: "#1d232f", display: "flex", height: "93%" }}>
+		// 	<Typography variant="h1" component="h2">
+		// 		h1. Heading
+		// 	</Typography>
+		// 	;
+		// </Box>
+	);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+	type tabs = "published" | "draft";
 
-export default App
+	const handleTabOnChange = (_e: React.SyntheticEvent, _newTab: tabs) => {};
+
+	return (
+		<>
+			{/* <AppBar sx={{ bgcolor: "#1d232f", height: "4vh" }}>aaaaaaaaa</AppBar> */}
+			<Box sx={{ bgcolor: "#1d232f", height: "4vh", width: "100vw", color: "white" }}>aaaaaaaaa</Box>
+			<Box sx={{ flexGrow: 1 }}>
+				<Grid container height={"91vh"}>
+					<Grid item xs={0.5} sx={{ bgcolor: "#141820", color: "white" }}>
+						aaaaa
+					</Grid>
+					<Grid item xs={2} sx={{ bgcolor: "#171c26", color: "white" }}>
+						<List>
+							<ListItem disablePadding>
+								<VscChevronDown />
+								<ListItemText primary={"Curriculum"} sx={{ color: "white", ml: 1 }} />
+							</ListItem>
+							<ListItem disablePadding>
+								<ListItemButton>
+									<i className="devicon-react-original colored"></i>
+									<ListItemText primary={"sobre.tsx"} sx={{ color: "white", ml: 1 }} />
+								</ListItemButton>
+							</ListItem>
+							<ListItem disablePadding>
+								<ListItemButton>
+									<i className="devicon-typescript-plain colored"></i>
+									<ListItemText primary={"contato.ts"} sx={{ color: "white", ml: 1 }} />
+								</ListItemButton>
+							</ListItem>
+
+							<ListItem disablePadding>
+								<ListItemButton>
+									<i className="devicon-python-plain colored"></i>
+									<ListItemText primary={"stack.ipynb"} sx={{ color: "white", ml: 1 }} />
+								</ListItemButton>
+							</ListItem>
+							<ListItem disablePadding>
+								<ListItemButton>
+									<i className="devicon-ruby-plain colored"></i>
+									<ListItemText primary={"habilidades.rb"} sx={{ color: "white", ml: 1 }} />
+								</ListItemButton>
+							</ListItem>
+							<ListItem disablePadding>
+								<ListItemButton>
+									<i className="devicon-javascript-plain colored"></i>
+									<ListItemText primary={"projetos.js"} sx={{ color: "white", ml: 1 }} />
+								</ListItemButton>
+							</ListItem>
+						</List>
+					</Grid>
+					<Grid item xs={9.5} sx={{ bgcolor: "#171c26", color: "white" }}>
+						<Tabs
+							value={currentTab}
+							onChange={handleTabOnChange}
+							textColor="inherit"
+							// indicatorColor="primary"
+							aria-label="secondary tabs example"
+						>
+							<Tab value="published" label="Publicadas" />
+							<Tab value="draft" label="Não Publicadas" />
+						</Tabs>
+						{abc}
+					</Grid>
+				</Grid>
+			</Box>
+		</>
+	);
+};
+
+export default App;
---
+++ curriculum/src/components/contact.tsx
@@ -0,0 +1,27 @@
+import { Box } from "@mui/system";
+// import { CopyBlock, atomOneDark, dracula, a11yDark } from "react-code-blocks";
+// import { CodeSection } from "react-code-section-lib";
+
+const Contact = () => {
+	// const value = `class Contanto {
+	//     email: a.ferrari.iaquinta@gmail.com
+	// }`;
+
+	return (
+		<Box>
+			{/* <CodeSection showLineNumbers={true} theme={"dark"} lang={"typescript"}>
+				{value}
+			</CodeSection> */}
+			{/* <CopyBlock
+				text={value}
+				language={"typescript"}
+				showLineNumbers={true}
+				startingLineNumber={1}
+				theme={a11yDark}
+				codeBlock
+			/> */}
+		</Box>
+	);
+};
+
+export default Contact;
