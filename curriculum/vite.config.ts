04157e92fed6503abdcc355daa679cac6fa7f349
AmandaFI <a.ferrari.iaquinta@gmail.com>
2023-07-16 19:50:07 -0300

    Deploy.

--- curriculum/vite.config.ts
+++ curriculum/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/amandafi.github.io",
+	base: "/amandafi.github.io/",
 	plugins: [react()],
 });
